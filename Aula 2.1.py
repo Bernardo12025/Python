f809b00d2c22f64a1e6fc4b2b41a27689374dbdd
Bernardo12025 <shark12025@sharkcoders.pt>
2025-09-29 20:11:31 +0100

    Add files via upload

---
+++ Aula 2.1.py
@@ -0,0 +1,5 @@
+nome = input("Qual é o nome do aluno?")
+
+curso = input("És aluno de que curso?")
+
+print(f'Boas Vindas "{nome}". És aluno do curso de "{curso}".')
---
+++ Aula 2.2 (Médias).py
@@ -0,0 +1,7 @@
+nota1 = (int(input("Qual a nota1 do aluno?")))
+
+nota2 = (int(input("Qual a nota2 do aluno?")))
+
+media = (nota1 + nota2)/2
+
+print(f'A média do aluno é {media}')
