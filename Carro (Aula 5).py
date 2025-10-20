979c2d299e9fa85783668f6b54fe47dfdf24813c
Bernardo12025 <shark12025@sharkcoders.pt>
2025-10-20 19:08:53 +0100

    Add files via upload

---
+++ Carro (Aula 5).py
@@ -0,0 +1,14 @@
+idadecarro = int(input("Quantos anos tem o carro?"))
+
+if(idadecarro>10):
+
+ print("O carro é antigo.")
+
+elif (idadecarro>5):
+    print("O carro é seminovo.")
+
+else:
+    print("O carro é novo.")
+
+
+
